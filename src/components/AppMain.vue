ebd4b4c63cd44ee6070e2d73d4982313d7b9d48b
sollendy <edlannino@tutanota.com>
2023-04-13 15:22:05 +0200

    inizio sezione 5 del main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,18 +18,18 @@
                     <h3 class="fw-bold">Meet The New Agency SEO Template From The Avada Team</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolore nostrum magni eos facilis error.</p>
                 </div>
-                <div id="card-cont">
-                    <div class="cards-up">
+                <div class="card-cont">
+                    <div class="cards">
                         <div><i class="fa-solid fa-road fs-4 icone-top-main"></i></div>
                         <h4 class="fw-bold">Establish Goals</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
-                    <div class="cards-up">
+                    <div class="cards">
                         <div><i class="fa-solid fa-video fs-4 icone-top-main"></i></div>
                         <h4 class="fw-bold">Work With a Team</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
-                    <div class="cards-up">
+                    <div class="cards">
                         <div><i class="fa-solid fa-droplet fs-4 icone-top-main"></i></div>
                         <h4 class="fw-bold">Get Results</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
@@ -42,33 +42,96 @@
         </section>
         <section id="seconda-sec">
             <div class="sec-cnt d-flex flex-column align-items-center">
-                <div id="up-sec" class="text-center">
+                <div class="up-sec text-center">
                     <h3 class="fw-bold">See Our Top Notch Services</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolore nostrum</p>
                 </div>
-                <div id="card-cont" class="d-flex gap-3 text-center">
-                    <div class="cards-up d-flex flex-column gap-3">
+                <div class="card-cont d-flex gap-3 text-center">
+                    <div class="cards d-flex flex-column gap-3">
                         <div><i class="fa-brands fa-google fs-4 icone-main-2"></i></div>
                         <h4 class="fw-bold">Google SEO</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
-                    <div class="cards-up d-flex flex-column gap-3">
+                    <div class="cards d-flex flex-column gap-3">
                         <div><i class="fa-solid fa-gear fs-4 icone-main-2"></i></div>
                         <h4 class="fw-bold">Brand Strategy</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
-                    <div class="cards-up d-flex flex-column gap-3">
+                    <div class="cards d-flex flex-column gap-3">
                         <div><i class="fa-solid fa-building fs-4 icone-main-2"></i></div>
                         <h4 class="fw-bold">Local SEO</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
-                    <div class="cards-up d-flex flex-column gap-3">
+                    <div class="cards d-flex flex-column gap-3">
                         <div><i class="fa-solid fa-chart-column fs-4 icone-main-2"></i></div>
                         <h4 class="fw-bold">SEO Analysis</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laborum quis dolor suscipit ad et voluptas facere voluptate.</p>
                     </div>
                 </div>
             </div>
+        </section>
+        <section id="terza-sec">
+            <div class="sec-cnt d-flex flex-column align-items-center">
+                <div class="up-sec text-center">
+                    <h3 class="fw-bold">Our Work</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolore nostrum</p>
+                </div>
+                <div class="foto-cnt d-flex">
+                    <div class="foto"><img src="../assets/case-study-gallery-3-1-800x600.jpg" alt=""></div>
+                    <div class="foto"><img src="../assets/case-study-gallery-2-800x600.jpg" alt=""></div>
+                    <div class="foto"><img src="../assets/case-study-gallery-1-1-800x600.jpg" alt=""></div>
+                    <div class="foto"><img src="../assets/case-study-gallery-4-1-800x600.jpg" alt=""></div>
+                    <div class="foto"><img src="../assets/case-study-gallery-5-1-800x600.jpg" alt=""></div>
+                    <div class="foto"><img src="../assets/case-study-gallery-6-1-800x600.jpg" alt=""></div>
+                </div>
+                <div class="end-btn">
+                    <button>View Our Work</button>
+                </div>
+            </div>
+        </section>
+        <section id="quarta-sec">
+            <div class="sec-cnt d-flex flex-column align-items-center">
+                <div class="up-sec text-center">
+                    <h3 class="fw-bold">Our Plans</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolore nostrum</p>
+                </div>
+                <div class="card-cont d-flex justify-content-between">
+                    <div class="cards d-flex flex-column align-items-center gap-3">
+                        <h4 class="fw-bold">Standard</h4>
+                        <p class="text-center">5 Projects</p>
+                        <p class="text-center">5 GB Storage</p>
+                        <p class="text-center">Unlimited users</p>
+                        <button>START TODAY</button>
+                    </div>
+                    <div class="cards d-flex flex-column align-items-center gap-3">
+                        <h4 class="fw-bold">Premium</h4>
+                        <p class="text-center">10 Projects</p>
+                        <p class="text-center">15 GB Storage</p>
+                        <p class="text-center">Unlimited users</p>
+                        <button>START TODAY</button>
+                    </div>
+                    <div class="cards d-flex flex-column align-items-center gap-3">
+                        <h4 class="fw-bold">Professional</h4>
+                        <p class="text-center">15 Projects</p>
+                        <p class="text-center">30 GB Storage</p>
+                        <p class="text-center">Unlimited users</p>
+                        <button>START TODAY</button>
+                    </div>
+                    <div class="cards d-flex flex-column align-items-center gap-3">
+                        <h4 class="fw-bold">Extreme</h4>
+                        <p class="text-center">Unlimited Projects</p>
+                        <p class="text-center">Unlimited Storage</p>
+                        <p class="text-center">Unlimited users</p>
+                        <button>START TODAY</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="quinta-sec">
+            <div class="sec-cont">
+                
+            </div>
+            <h1>salve a tutti dalla quinta sezione</h1>
         </section>
     </main>
 </template>
@@ -86,11 +149,11 @@
             padding: 5% 0 4% 0;
             text-align: center;
             }
-            #card-cont {
-            display: flex;
-            justify-content: space-between;
+            .card-cont {
+                display: flex;
+                justify-content: space-between;
             // gap: 1.5%;
-            .cards-up {
+                .cards {
                 padding: 3%;
                 text-align: center;
                 width: 30%;
@@ -98,13 +161,13 @@
                 display: flex;
                 flex-flow: column;
                 gap: .7em;
-                div {
-                    .icone-top-main {
+                    div {
+                        .icone-top-main {
                         color: rgb(255, 167, 55);
+                        }
                     }
                 }
             }
-        }
         }
         
     }
@@ -115,15 +178,16 @@
         .sec-cnt {
             width: 75%;
             margin: auto;
+            padding-bottom: 7%;
             // background-color: blue;
             color: white;
-            #up-sec {
+            .up-sec {
                 width: 40%;
                 padding: 5% 0 4% 0;
                 text-align: center;
             }
-            #card-cont {
-                .cards-up {
+            .card-cont {
+                .cards {
                     background-color: rgb(7, 27, 80);
                     padding: 3%;
                     div {
@@ -131,6 +195,57 @@
                             color: rgb(255, 167, 55);
                         }
                     }
+                }
+            }
+        }
+    }
+    #terza-sec {
+        .sec-cnt {
+            width: 75%;
+            margin: auto;
+            .up-sec {
+            width: 45%;
+            padding: 5% 0 4% 0;
+            text-align: center;
+            }
+            .foto-cnt {
+                flex-flow: row wrap;
+                justify-content: space-between;
+                gap: 0.5%;
+                .foto {
+                    width: 33%;
+                    padding-bottom: 1%;
+                    img{
+                        width: 100%;
+                    }
+                }
+            }
+            .end-btn {
+                padding: 5% 0 4.5% 0;
+            }
+        }
+    }
+    #quarta-sec {
+        background-image: url('../assets/background1.jpg');
+        .sec-cnt {
+            width: 75%;
+            margin: auto;
+            .up-sec {
+                width: 45%;
+                padding: 5% 0 4% 0;
+                text-align: center;
+                color: white;
+            }
+            .card-cont {
+                width: 100%;
+                padding-bottom: 7%;
+                // display: flex;
+                // justify-content: space-between;
+                // gap: 2%;
+                .cards {
+                    width: 24%;
+                    padding: 2%;
+                    background-color: rgb(255, 255, 255);
                 }
             }
         }
